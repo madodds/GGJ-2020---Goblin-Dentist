88958b1a1305ee3778db11c92f27bd48fef03fe2
Matthew Dodds <madodds@gmail.com>
2020-02-01 21:14:15 -0700

    updated cool scripts

--- Goblin Dentist/Assets/Scripts/DerpyHead.cs
+++ Goblin Dentist/Assets/Scripts/DerpyHead.cs
@@ -9,15 +9,15 @@ public class DerpyHead : Head
 
     public override HeadType headType => HeadType.Derpy;
 
-    protected override List<(Vector2 position, Vector2 scale, Tooth.ToothArea toothArea, int layerOrder)> toothData =>
-        new List<(Vector2 position, Vector2 scale, Tooth.ToothArea toothArea, int layerOrder)>
+    protected override List<(Vector3 position, Vector3 scale, Tooth.ToothArea toothArea, int layerOrder)> toothData =>
+        new List<(Vector3 position, Vector3 scale, Tooth.ToothArea toothArea, int layerOrder)>
         {
-            (new Vector2(0.53f, -6.25f), new Vector3(0.45f, 0.45f), Tooth.ToothArea.LowerRight, 3),
-            (new Vector2(1.62f, -6.13f), new Vector3(0.45f, 0.45f), Tooth.ToothArea.LowerLeft, 3),
-            (new Vector2(2.44f, -5.79f), new Vector3(0.41f, 0.41f), Tooth.ToothArea.LowerRight, 2),
-            (new Vector2(-0.51f, -5.98f), new Vector3(0.41f, 0.41f), Tooth.ToothArea.LowerLeft, 2),
-            (new Vector2(-1.18f, -4.99f), new Vector3(0.38f, 0.38f), Tooth.ToothArea.LowerRight, 1),
-            (new Vector2(3.18f, -4.78f), new Vector3(0.38f, 0.38f), Tooth.ToothArea.LowerRight, 1)
+            (new Vector3(0.53f, -6.25f), new Vector3(0.45f, 0.45f), Tooth.ToothArea.LowerRight, 3),
+            (new Vector3(1.62f, -6.13f), new Vector3(0.45f, 0.45f), Tooth.ToothArea.LowerLeft, 3),
+            (new Vector3(2.44f, -5.79f), new Vector3(0.41f, 0.41f), Tooth.ToothArea.LowerRight, 2),
+            (new Vector3(-0.51f, -5.98f), new Vector3(0.41f, 0.41f), Tooth.ToothArea.LowerLeft, 2),
+            (new Vector3(-1.18f, -4.99f), new Vector3(0.38f, 0.38f), Tooth.ToothArea.LowerRight, 1),
+            (new Vector3(3.18f, -4.78f), new Vector3(0.38f, 0.38f), Tooth.ToothArea.LowerRight, 1)
         };
 
     protected override void Start()
--- Goblin Dentist/Assets/Scripts/Head.cs
+++ Goblin Dentist/Assets/Scripts/Head.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,13 +17,14 @@ public abstract class Head : MonoBehaviour
 
     protected abstract int badToothProbability { get; }
 
-    protected abstract List<(Vector2 position, Vector2 scale, Tooth.ToothArea toothArea, int layerOrder)> toothData { get; }
+    protected abstract List<(Vector3 position, Vector3 scale, Tooth.ToothArea toothArea, int layerOrder)> toothData { get; }
 
     protected virtual void Start()
     {
         foreach (var data in toothData)
         {
-            GameObject newTooth = Instantiate(tooth);
+            GameObject newTooth = Instantiate(tooth, transform);
+            newTooth.name = $"Tooth:{Enum.GetName(typeof(Tooth.ToothArea), data.toothArea)}.{data.layerOrder}";
             Tooth toothComp = newTooth.GetComponent<Tooth>();
             toothComp.Init(data.position, data.scale, data.toothArea, data.layerOrder, badToothProbability);
         }
