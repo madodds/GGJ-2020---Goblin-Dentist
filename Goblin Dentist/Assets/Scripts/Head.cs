e327cb6c5f3f3f1ea45a656dec88ba796e4aabc6
Matthew Dodds <madodds@gmail.com>
2020-02-01 18:33:45 -0700

    adding scripts baby

---
+++ Goblin Dentist/Assets/Scripts/Head.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Head : MonoBehaviour
+{
+    public enum HeadType
+    {
+        Pointy,
+        Scary
+    }
+
+    public List<Tooth> teeth;
+
+    public abstract string SpriteLocation { get; }
+
+    public abstract HeadType headType { get; }
+
+    public abstract void Init(int badToothProbability);
+
+    public GameObject tooth;
+}
---
+++ Goblin Dentist/Assets/Scripts/HeadManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadManager : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    Head currentHead;
+    public GameObject[] possibleHeads;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("here");
+        Instantiate(possibleHeads[0], this.transform);
+        //currentHead.Init(50);
+        //spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        //spriteRenderer.sprite = Resources.Load<Sprite>(currentHead.SpriteLocation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
