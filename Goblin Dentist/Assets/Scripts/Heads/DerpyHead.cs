2dffba8851c6dada67d8dc6b45526a3f2cf86e4d
Matthew Dodds <madodds@gmail.com>
2020-02-02 02:50:45 -0700

    updated teeth locations

--- Goblin Dentist/Assets/Scripts/Heads/DerpyHead.cs
+++ Goblin Dentist/Assets/Scripts/Heads/DerpyHead.cs
@@ -12,19 +12,19 @@ public class DerpyHead : Head
     protected override List<(Vector3 position, Vector3 scale, Tooth.ToothArea toothArea, int layerOrder)> toothData =>
         new List<(Vector3 position, Vector3 scale, Tooth.ToothArea toothArea, int layerOrder)>
         {
-            (new Vector3(0.61f, -6.26f),   new Vector3(1.285f, 1.683f), Tooth.ToothArea.Lower, 3),
-            (new Vector3(-0.66f, -6.67f), new Vector3(1.419f, 1.37f), Tooth.ToothArea.Lower, 3),
-            (new Vector3(1.84f, -5.23f),  new Vector3(1.365f, 1.327f), Tooth.ToothArea.Lower, 2),
-            (new Vector3(-1.81f, -6.15f), new Vector3(1.586f, 1.552f), Tooth.ToothArea.Lower, 2),
-            (new Vector3(2.48f, -3.98f),  new Vector3(1.545f, 1.409f), Tooth.ToothArea.Lower, 1),
-            (new Vector3(-2.7f, -4.86f),  new Vector3(1.225f, 1.243f), Tooth.ToothArea.Lower, 1),
+            (new Vector3(0.61f, -6.26f),   new Vector3(1.285f, 2.383f), Tooth.ToothArea.Lower, 4),
+            (new Vector3(-0.66f, -6.67f), new Vector3(1.419f, 2.17f), Tooth.ToothArea.Lower, 4),
+            (new Vector3(1.84f, -5.23f),  new Vector3(1.365f, 2.127f), Tooth.ToothArea.Lower, 3),
+            (new Vector3(-1.81f, -6.15f), new Vector3(1.586f, 2.152f), Tooth.ToothArea.Lower, 3),
+            (new Vector3(2.48f, -3.98f),  new Vector3(1.545f, 2.109f), Tooth.ToothArea.Lower, 2),
+            (new Vector3(-2.7f, -4.86f),  new Vector3(1.225f, 2.143f), Tooth.ToothArea.Lower, 2),
 
-            (new Vector3(0.39f, -3.87f),  new Vector3(2.103f, 1.583f), Tooth.ToothArea.Upper, 3),
-            (new Vector3(-1.22f, -3.5f), new Vector3(1.998f, 1.49f), Tooth.ToothArea.Upper, 3),
-            (new Vector3(1.69f, -3.36f),  new Vector3(1.601f, 1.431f), Tooth.ToothArea.Upper, 2),
-            (new Vector3(-2.66f, -3.2f), new Vector3(1.496f, 1.366f), Tooth.ToothArea.Upper, 2),
-            (new Vector3(-3.8f, -2.82f), new Vector3(1.389f, 1.344f), Tooth.ToothArea.Upper, 1),
-            (new Vector3(3.2f, -2.49f),   new Vector3(1.721f, 1.251f), Tooth.ToothArea.Upper, 1)
+            (new Vector3(0.39f, -3.87f),  new Vector3(2.103f, 2.383f), Tooth.ToothArea.Upper, 4),
+            (new Vector3(-1.22f, -3.5f), new Vector3(1.998f, 2.19f), Tooth.ToothArea.Upper, 4),
+            (new Vector3(1.69f, -3.36f),  new Vector3(1.601f, 2.131f), Tooth.ToothArea.Upper, 3),
+            (new Vector3(-2.66f, -3.2f), new Vector3(1.496f, 2.166f), Tooth.ToothArea.Upper, 3),
+            (new Vector3(-3.8f, -2.82f), new Vector3(1.389f, 2.144f), Tooth.ToothArea.Upper, 2),
+            (new Vector3(3.2f, -2.49f),   new Vector3(1.721f, 2.151f), Tooth.ToothArea.Upper, 2)
         };
 
     protected override void Start()
--- Goblin Dentist/Assets/Scripts/Heads/RoundHead.cs
+++ Goblin Dentist/Assets/Scripts/Heads/RoundHead.cs
@@ -12,19 +12,19 @@ public class RoundHead : Head
     protected override List<(Vector3 position, Vector3 scale, Tooth.ToothArea toothArea, int layerOrder)> toothData =>
         new List<(Vector3 position, Vector3 scale, Tooth.ToothArea toothArea, int layerOrder)>
         {
-            (new Vector3(0.57f, -4.71f),   new Vector3(1.285f, 1.683f), Tooth.ToothArea.Lower, 3),
-            (new Vector3(-0.65f, -4.86f), new Vector3(1.419f, 1.37f), Tooth.ToothArea.Lower, 3),
-            (new Vector3(1.43f, -4.5f),  new Vector3(1.365f, 1.327f), Tooth.ToothArea.Lower, 2),
-            (new Vector3(-1.73f, -4.22f), new Vector3(1.586f, 1.552f), Tooth.ToothArea.Lower, 2),
-            (new Vector3(2.21f, -3.93f),  new Vector3(1.545f, 1.409f), Tooth.ToothArea.Lower, 1),
-            (new Vector3(-2.62f, -3.48f),  new Vector3(1.225f, 1.243f), Tooth.ToothArea.Lower, 1),
+            (new Vector3(0.57f, -4.71f),   new Vector3(1.285f, 1.683f), Tooth.ToothArea.Lower, 4),
+            (new Vector3(-0.65f, -4.86f), new Vector3(1.419f, 1.37f), Tooth.ToothArea.Lower, 4),
+            (new Vector3(1.43f, -4.5f),  new Vector3(1.365f, 1.327f), Tooth.ToothArea.Lower, 3),
+            (new Vector3(-1.73f, -4.22f), new Vector3(1.586f, 1.552f), Tooth.ToothArea.Lower, 3),
+            (new Vector3(2.21f, -3.93f),  new Vector3(1.545f, 1.409f), Tooth.ToothArea.Lower, 2),
+            (new Vector3(-2.62f, -3.48f),  new Vector3(1.225f, 1.243f), Tooth.ToothArea.Lower, 2),
 
-            (new Vector3(0.54f, -2.45f),  new Vector3(2.103f, 1.583f), Tooth.ToothArea.Upper, 3),
-            (new Vector3(-0.88f, -2.05f), new Vector3(1.998f, 1.49f), Tooth.ToothArea.Upper, 3),
-            (new Vector3(1.8f, -2.14f),  new Vector3(1.601f, 1.431f), Tooth.ToothArea.Upper, 2),
-            (new Vector3(-2.03f, -2.06f), new Vector3(1.496f, 1.366f), Tooth.ToothArea.Upper, 2),
-            (new Vector3(-3.12f, -2.72f), new Vector3(1.389f, 1.344f), Tooth.ToothArea.Upper, 1),
-            (new Vector3(3.04f, -2.42f),   new Vector3(1.721f, 1.251f), Tooth.ToothArea.Upper, 1)
+            (new Vector3(0.54f, -2.45f),  new Vector3(2.103f, 1.583f), Tooth.ToothArea.Upper, 4),
+            (new Vector3(-0.88f, -2.05f), new Vector3(1.998f, 1.49f), Tooth.ToothArea.Upper, 4),
+            (new Vector3(1.8f, -2.14f),  new Vector3(1.601f, 1.431f), Tooth.ToothArea.Upper, 3),
+            (new Vector3(-2.03f, -2.06f), new Vector3(1.496f, 1.366f), Tooth.ToothArea.Upper, 3),
+            (new Vector3(-3.12f, -2.72f), new Vector3(1.389f, 1.344f), Tooth.ToothArea.Upper, 2),
+            (new Vector3(3.04f, -2.42f),   new Vector3(1.721f, 1.251f), Tooth.ToothArea.Upper, 2)
         };
 
     protected override void Start()
